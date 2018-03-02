dc296700b6c0ab2df7945e017c4570b8df141448
George Haidar <ghaidar0@gmail.com>
2018-03-02 12:47:51 +0100

    add some comments to the server code

--- README.md
+++ README.md
@@ -17,6 +17,6 @@ A web app development stack including:
 * Full Typescript coverage across the codebase
 * GraphQL development using Apollo including proper typing using apollo-codegen
 * Styling thanks to styled-components
-* Offline support using Workbox through the [next-offline](https://github.com/hanford/next-offline) plugin
+* Offline support using Workbox through the [next-offline](https://github.com/hanford/next-offline) plugin (in production build `yarn build && yarn start`)
 * Webpack bundle analyzer integrated using [@zeit/next-bundle-analyzer](https://github.com/zeit/next-plugins/tree/master/packages/next-bundle-analyzer)
 * SSR support thanks to next.js
--- server/index.ts
+++ server/index.ts
@@ -21,19 +21,29 @@ const schema = makeExecutableSchema({
   resolvers
 });
 
+// GraphQL requires a single route
 app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL is a handy ui for browsing a graphql server like this one
+// Either install the GraphiQL app or go to
+// http://localhost:3000/graphiql (by default) to use the web ui
 app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
+// If you want to have dynamic routes you need to explicitly add server routes
+// like this route that renders a single post
 app.get("/posts/:id", (req, res) => {
   const { params, query } = req;
   nextApp.render(req, res, "/posts", Object.assign({}, params, query));
 });
 
+// The next-offline plugin requires registering a route for serving the
+// service worker script that will act as a cache
 app.get("/service-worker.js", (req, res) => {
   const filePath = path.join(__dirname, "..", ".next", "service-worker.js");
   nextApp.serveStatic(req, res, filePath);
 });
 
+// All other routes are handled by next.js
 app.get("*", (req, res) => nextHandler(req, res));
 
 export const start = async (port: string | number) => {
